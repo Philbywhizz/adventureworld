14103c4dd7381fac8e5d178538958c335d24bbd4
Phil Howlett <phil.g.howlett@gmail.com>
2014-05-03 22:40:33 +1000

    player now blinks and is drawn in the correct location on the screen

--- player.lua
+++ player.lua
@@ -5,6 +5,23 @@
 Player = Class{}
 
 function Player:init()
+
+	self.width = 16
+	self.height = 32
+	self.flash = 0
+	self.img = {
+		current = 1,
+		toggle = function() -- toggle between flashing player graphic
+			if self.img.current == 1 then
+				self.img.current = 2
+			else
+				self.img.current = 1
+			end
+		end,
+		love.graphics.newImage("res/person.png"),
+		love.graphics.newImage("res/iperson.png")
+	}
+
 end
 
 function Player:setXY(x, y)
@@ -16,8 +33,16 @@ function Player:getXY()
 	return self.x, self.y
 end
 
-function Player:draw()
+function Player:update(dt)
+	-- flash the player if required
+	self.flash = self.flash + dt
+	if self.flash > .25 then
+		self.flash = 0
+		self.img:toggle()
+	end
 end
 
-function Player:update(dt)
+function Player:draw()
+	love.graphics.draw(self.img[self.img.current], (self.x - 1) * self.width, (self.y - 1) * self.height)
 end
+
