08bf207ea2da5214d14f82f9c313013c858de258
Phil Howlett <phil.g.howlett@gmail.com>
2014-05-04 21:11:25 +1000

    Town layout - it isn't pretty but it is functional.

--- state/town.lua
+++ state/town.lua
@@ -18,7 +18,16 @@ end
 function Town:draw()
 	love.graphics.setColor(255, 255, 255) -- white
 	love.graphics.setFont(townFont)
-	center(4, "**** TOWN ****")
+	center(2, "**** TOWN ****")
+	center(4, "You have "..player:getCashBalance().." draques with you.")
+	center(5, "What would you like to do:")
+	center(8, "[1]  BUY SOME ARMOUR")
+	center(9, "[2]  BUY SOME WEAPONS")
+	center(10, "[3]  BUY FOON 'N STUFF")
+	center(11, "[4]  VISIT BANK")
+	center(13, "[5]  SHOW CHARACTER")
+	center(14, "[6]  HELP")
+	center(16, "[0]  EXIT TOWN")
 	center(20, "Press any key to continue")
 end
 
@@ -26,7 +35,13 @@ function Town:update(dt)
 end
 
 function Town:keypressed(key)
-	if key then
+	if key == "5" then
+		Gamestate.push(charsheet)
+	end
+	if key == "6" then
+		Gamestate.push(help)
+	end
+	if key == "0" then
 		Gamestate.pop()
 	end
 end
